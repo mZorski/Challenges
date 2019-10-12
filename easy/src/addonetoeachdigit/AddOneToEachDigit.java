378b69471ff3e50456b31b2a32030d5eacb073e2
mZorski <zorski.michal@wp.pl>
2019-10-12 12:45:16 +0200

    Rename package and class

--- easy/src/addonetoeachdigit/AddOneToEachDigit.java
+++ easy/src/addonetoeachdigit/AddOneToEachDigit.java
@@ -1,13 +1,11 @@
-package addnumberdigits;
-
-import java.util.Random;
+package addonetoeachdigit;
 
 /**
  * [2019-02-11] Challenge #375 [Easy] Print a new number by adding one to each of its digit
  * https://www.reddit.com/r/dailyprogrammer/comments/aphavc/20190211_challenge_375_easy_print_a_new_number_by/
  */
 
-public class AddNumberDigits {
+public class AddOneToEachDigit {
     public static void main(String[] args) {
         int[] numbers = {0, 9, 998, 999, 1893, 143321};
 
